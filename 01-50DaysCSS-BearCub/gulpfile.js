c03958feef454aa3a086836decf3606720db9f8b
ebectar <acebe424@gmail.com>
2018-09-27 16:55:31 -0600

    changing folder structure to remove dist and js, will not be using

--- 01-50DaysCSS-BearCub/gulpfile.js
+++ 01-50DaysCSS-BearCub/gulpfile.js
@@ -6,3 +6,7 @@ gulp.task('sass', function(){
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('./app/css'))
 });
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+})
