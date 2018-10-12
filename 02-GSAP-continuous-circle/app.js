b03a22546a6267615249114f39dc7789d7c25247
ebectar <acebe424@gmail.com>
2018-10-12 16:10:26 -0600

    adding more circle animations

--- 02-GSAP-continuous-circle/app.js
+++ 02-GSAP-continuous-circle/app.js
@@ -1,6 +1,8 @@
-let line = '.container div span'
-let animation = new TimelineMax({repeat: -1, repeatDelay: 1})
+let line = ".container div span";
+let tl = new TimelineMax({ repeat: -1, repeatDelay: 1 });
 
-animation.from(line, 1, {scale: 0})
-    .to(line, 1, {y: '-100px', scaleX: 0.25})
-    .timeScale(3);
+tl.from(line, 1, { scale: 0 })
+  .to(line, 1, { y: "-150em", scaleX: 0.25 })
+  .to(line, 1, { scaleX: 1 })
+  .to(line, 1, {borderRadius: "50%"})
+  .timelineScale(3);
