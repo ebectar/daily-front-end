d4720622e614338eb1f77bd4fcb26198f84baf48
ebectar <acebe424@gmail.com>
2018-11-04 02:44:56 -0700

    adding nose, mouth, paws

--- 01-50DaysCSS-BearCub/gulpfile.js
+++ 01-50DaysCSS-BearCub/gulpfile.js
@@ -1,23 +1,29 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')
 
-gulp.task('sass', function(){
-  return gulp.src('./app/scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('./app/css'))
+gulp.task('pug:build', function(){
+  return gulp.src('./*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public'))
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']); 
+gulp.task('sass:build', function(){
+  return gulp.src('./scss/*.scss')
+    .pipe(sass()).on('error', sass.logError)
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('pug:watch', () => {
+  return gulp
+      .watch('./src/html/*.pug', ['pug']);
+});
+
+gulp.task('sass:watch', function(){
+  gulp.watch('./scss/**/*.scss', ['sass']); 
 })
 
-    // .to(elements, 1, {scaleY: 0.25, rotation: 180})
-    // .to(elements, 1, {scaleX: 1})
-    // .to(elements, 1, {y: '-60px', scaleY: 0.1})
-    // .to(elements, 1, {x: '-30px'})
-    // .to(elements, 1, {x: '30px'})
-    // .to(elements, 1, {x: '0', scaleX: 0.1, scaleY: 1})
-    // .to(elements, 1, {scaleX: 0.5, scaleY: 0.1})
-    // .to(elements, 1, {y: '-80px', scaleY: 0.5, borderRadius: '50%'})
-    // .to(elements, 2, {y: '-10px', scaleX: 0.1, scaleY: 0.5, borderRadius: '0%', rotation: 0})
-    // .to(elements, 1, {y: '-300px', delay: 0.5})
+gulp.task('pug', ['pug:watch', 'pug:build']);
+gulp.task('sass', ['sass:watch', 'sass:build']);
