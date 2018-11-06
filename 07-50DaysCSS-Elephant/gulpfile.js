3c517702f7146dfe22d8455940920a75fb8b3672
ebectar <acebe424@gmail.com>
2018-11-06 15:28:01 -0700

    adding base index.html for deploy, adding new projects

---
+++ 07-50DaysCSS-Elephant/gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')
+
+gulp.task('pug:build', function(){
+  return gulp.src('./*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./public'))
+});
+
+gulp.task('sass:build', function(){
+  return gulp.src('./scss/*.scss')
+    .pipe(sass()).on('error', sass.logError)
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('pug:watch', () => {
+  return gulp
+      .watch('./*.pug', ['pug']);
+});
+
+gulp.task('sass:watch', function(){
+  gulp.watch('./scss/**/*.scss', ['sass']); 
+})
+
+gulp.task('pug', ['pug:watch', 'pug:build']);
+gulp.task('sass', ['sass:watch', 'sass:build']);
