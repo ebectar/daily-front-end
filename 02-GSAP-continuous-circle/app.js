b1172ec707fc75e43866b8273dd47335e7189520
ebectar <acebe424@gmail.com>
2018-11-06 09:30:28 -0700

    recompiling html

--- 02-GSAP-continuous-circle/app.js
+++ 02-GSAP-continuous-circle/app.js
@@ -5,4 +5,5 @@ tl.from(line, 1, { scale: 0 })
   .to(line, 1, { y: "-150em", scaleX: 0.25 })
   .to(line, 1, { scaleX: 1 })
   .to(line, 1, {borderRadius: "50%"})
+  .to(elements, 1, {y: '-300px', delay: 0.5})
   .timelineScale(3);
