ed0b6199c46b3682bb38dbd4c7b24030635e8cea
ebectar <acebe424@gmail.com>
2019-01-29 20:44:42 -0700

    started constructing legs

--- 06-50DaysCSS-Elephant/gulpfile.js
+++ 06-50DaysCSS-Elephant/gulpfile.js
@@ -16,7 +16,7 @@ function css() {
   return (
     gulp.src("scss/*.scss")
     .pipe(sass()).on("error", sass.logError)
-    .pipe(gulp.dest("styles"))
+    .pipe(gulp.dest("./public/css/"))
   );
 }
 
